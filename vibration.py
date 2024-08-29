157915045c82033407ab2bcdcdbb23e492758612
jojo_golden_wings <jozeftunisino@gmail.com>
2024-08-29 16:59:41 +0200

    vibration_2

--- vibration.py
+++ vibration.py
@@ -2,21 +2,21 @@ import RPi.GPIO as GPIO
 import time
 
 # Set up GPIO
-SENSOR_INPUT = 4  # GPIO4
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(SENSOR_INPUT, GPIO.IN)
+GPIO.setmode(GPIO.BCM)  # Use BCM numbering
+GPIO.setup(16, GPIO.IN)  # Set GPIO4 as input
 
 try:
     while True:
-        if GPIO.input(SENSOR_INPUT) == 0:
-            print(1)  # Vibration detected
+        vibration_detected = GPIO.input(16)
+        if vibration_detected:
+            #print(1)  # Vibration detected
+            vibration_detected
         else:
             print(0)  # No vibration
-        time.sleep(0.5)  # Adjust the delay as needed
+        time.sleep(0.1)  # Adjust the delay as needed
 
 except KeyboardInterrupt:
     print("Exiting program")
-
+    
 finally:
-    GPIO.cleanup()
+    GPIO.cleanup()  # Clean up GPIO settings
