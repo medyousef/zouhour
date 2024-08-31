ee10ad8f637e668f66b6d7818dc73b2e9a5d0e07
jojo_golden_wings <jozeftunisino@gmail.com>
2024-08-31 19:06:50 +0200

    vibration_2

--- vibration.py
+++ vibration.py
@@ -8,11 +8,8 @@ GPIO.setup(16, GPIO.IN)  # Set GPIO4 as input
 try:
     while True:
         vibration_detected = GPIO.input(16)
-        if vibration_detected == 1:
             #print(1)  # Vibration detected
-           print("value detected " + str(vibration_detected))
-        elif vibration_detected == 0:
-            print("value not detected " + str(vibration_detected))
+        print("value detected " + str(vibration_detected))
         #    print(0)  # No vibration
         time.sleep(1)  # Adjust the delay as needed
 
