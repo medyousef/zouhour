0747512b0f1a5f5cbee7a3ba1c78044b777824d6
jojo_golden_wings <jozeftunisino@gmail.com>
2024-08-31 19:21:40 +0200

    vibration_2

--- vibration.py
+++ vibration.py
@@ -3,15 +3,21 @@ import time
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)  # Use BCM numbering
-GPIO.setup(16, GPIO.IN)  # Set GPIO4 as input
+GPIO.setup(16, GPIO.IN)  # Set GPIO16 as input
 
 try:
+    detection_values = []
+    start_time = time.time()
+
     while True:
         vibration_detected = GPIO.input(16)
-            #print(1)  # Vibration detected
-        print("value detected " + str(vibration_detected))
-        #    print(0)  # No vibration
-        time.sleep(0.1)  # Adjust the delay as needed
+        detection_values.append(vibration_detected)
+
+        if time.time() - start_time >= 2:
+            mean_value = sum(detection_values) / len(detection_values)
+            print("Mean value detected: {:.2f}".format(mean_value))
+            detection_values = []  # Reset the list
+            start_time = time.time()  # Reset the timer
 
 except KeyboardInterrupt:
     print("Exiting program")
