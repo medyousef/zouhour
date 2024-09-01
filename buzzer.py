d75b154df6628855829ef4bde0d4774b00e2df85
jojo_golden_wings <jozeftunisino@gmail.com>
2024-09-01 19:59:43 +0200

    buzz alternance

--- buzzer.py
+++ buzzer.py
@@ -12,12 +12,11 @@ pwm = GPIO.PWM(buzzer_pin, 3000)  # Initialize PWM with a fixed frequency
 
 try:
     while True:
-        # Buzz for 3 seconds
-        pwm.start(50)  # Start buzzing
-        time.sleep(3)
-
         # Stop buzzing
         pwm.stop()  # Stop buzzing
+        time.sleep(3)
+        # Buzz for 3 seconds
+        pwm.start(50)  # Start buzzing
         time.sleep(3)
 
 except KeyboardInterrupt:
