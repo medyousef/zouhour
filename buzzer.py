8b0b61363a73e18ed83eb74b18447222b8d0638e
jojo_golden_wings <jozeftunisino@gmail.com>
2024-09-01 20:08:20 +0200

    buzz alternance

--- buzzer.py
+++ buzzer.py
@@ -5,7 +5,9 @@ import time
 buzzer_pin = 4
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(buzzer_pin, GPIO.OUT)
 pwm = GPIO.PWM(buzzer_pin, 3000)  # Initialize PWM with a fixed frequency
+
 
 try:
     while True:
