042dd1cfdc445095a241168a7e9dc8cdae1b1be8
jojo_golden_wings <jozeftunisino@gmail.com>
2024-09-01 20:44:57 +0200

    buttons

--- button_handler.py
+++ button_handler.py
@@ -6,8 +6,8 @@ BUTTON_PAUSE_PIN = 27 # Button for "pause"
 BUTTON_PANNE_PIN = 17  # Button for "panne"
 BUTTON_CHANGEMENT_PIN = 22  # Button for "changement"
 BUTTON_PRODUCTION_PIN = 5  # Button for "production"
-BUTTON_REGLAGE_PIN = 6  # Button for "reglage"
-BUTTON_ORGANISATION_PIN = 13  # Button for "organisation"
+BUTTON_REGLAGE_PIN = 13  # Button for "reglage"
+BUTTON_ORGANISATION_PIN = 6  # Button for "organisation"
 
 def setup_buttons():
     GPIO.setup(BUTTON_PRODUCTION_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Button input for production
