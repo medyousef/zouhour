f7441f43a48c6c3c4914365593ed39d4b560892c
jojo_golden_wings <jozeftunisino@gmail.com>
2024-09-01 20:07:43 +0200

    buzz alternance

--- buzzer.py
+++ buzzer.py
@@ -5,22 +5,17 @@ import time
 buzzer_pin = 4
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(buzzer_pin, GPIO.OUT)
+pwm = GPIO.PWM(buzzer_pin, 3000)  # Initialize PWM with a fixed frequency
 
 try:
     while True:
         # Start buzzing
-        pwm = GPIO.PWM(buzzer_pin, 3000)  # Initialize PWM with a fixed frequency
         pwm.start(50)  # Start PWM with a 50% duty cycle (buzzer on)
         time.sleep(3)  # Buzz for 3 seconds
+
         # Stop buzzing completely
         pwm.stop()  # Stop the PWM, turning the buzzer off
         time.sleep(3)  # Pause for 3 seconds with the buzzer off
-        pwm = GPIO.PWM(buzzer_pin, 3000)
-        pwm.start(50)  # Start PWM with a 50% duty cycle (buzzer on)
-        time.sleep(3)  # Buzz for 3 seconds
-        pwm.stop()  # Stop the PWM, turning the buzzer off
-        time.sleep(3)  # Pause for 3 seconds with the buzzer off
-        pwm = GPIO.PWM(buzzer_pin, 3000)
+
 except KeyboardInterrupt:
     GPIO.cleanup()
