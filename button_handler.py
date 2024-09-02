61d615e1f494b1db254a8be76f321829daab05c0
jojo_golden_wings <jozeftunisino@gmail.com>
2024-09-02 22:04:55 +0200

    pause/panne soustraire

--- button_handler.py
+++ button_handler.py
@@ -4,8 +4,8 @@ import time
 # Define GPIO for buttons
 BUTTON_PAUSE_PIN = 27 # Button for "pause"
 BUTTON_PANNE_PIN = 17  # Button for "panne"
-BUTTON_CHANGEMENT_PIN =5   # Button for "changement"
-BUTTON_PRODUCTION_PIN =22   # Button for "production"
+BUTTON_CHANGEMENT_PIN = 22  # Button for "changement"
+BUTTON_PRODUCTION_PIN = 5  # Button for "production"
 BUTTON_REGLAGE_PIN = 13  # Button for "reglage"
 BUTTON_ORGANISATION_PIN = 6  # Button for "organisation"
 
