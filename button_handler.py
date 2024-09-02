258b1386a4f558f224b8fe1c33ea0c4064e48168
jojo_golden_wings <jozeftunisino@gmail.com>
2024-09-02 23:40:07 +0200

    is_machine_on

--- button_handler.py
+++ button_handler.py
@@ -3,9 +3,9 @@ import time
 
 # Define GPIO for buttons
 BUTTON_PAUSE_PIN = 27 # Button for "pause"
-BUTTON_PANNE_PIN = 17  # Button for "panne"
-BUTTON_CHANGEMENT_PIN =5   # Button for "changement"
-BUTTON_PRODUCTION_PIN =22   # Button for "production"
+BUTTON_PANNE_PIN = 5  # Button for "panne"
+BUTTON_CHANGEMENT_PIN = 17   # Button for "changement"
+BUTTON_PRODUCTION_PIN = 22   # Button for "production"
 BUTTON_REGLAGE_PIN = 13  # Button for "reglage"
 BUTTON_ORGANISATION_PIN = 6  # Button for "organisation"
 VIBRATION_PIN = 16
