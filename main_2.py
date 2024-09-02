fc7c3ad00ec717b4c2b9bf41f5375dfb5b5b3375
jojo_golden_wings <jozeftunisino@gmail.com>
2024-09-03 01:25:28 +0200

    pause/panne soustraire

--- main_2.py
+++ main_2.py
@@ -33,7 +33,7 @@ def main():
         vibration_detected = GPIO.input(16)
         detection_values.append(vibration_detected)
         #if time.time() - start_time >= 1:
-        
+        print(str(vibration_detected))
         mean_value = sum(detection_values) / len(detection_values)
         print("Mean value detected: {:.5f}".format(mean_value))
         detection_values = []  # Reset the list
