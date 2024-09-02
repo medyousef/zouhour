b7c42010c86fb2075d0328cb7aad12b931985473
jojo_golden_wings <jozeftunisino@gmail.com>
2024-09-02 23:15:37 +0200

    is_machine_on

--- button_handler.py
+++ button_handler.py
@@ -8,6 +8,7 @@ BUTTON_CHANGEMENT_PIN =5   # Button for "changement"
 BUTTON_PRODUCTION_PIN =22   # Button for "production"
 BUTTON_REGLAGE_PIN = 13  # Button for "reglage"
 BUTTON_ORGANISATION_PIN = 6  # Button for "organisation"
+VIBRATION_PIN = 16
 
 def setup_buttons():
     GPIO.setup(BUTTON_PRODUCTION_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Button input for production
@@ -16,4 +17,5 @@ def setup_buttons():
     GPIO.setup(BUTTON_CHANGEMENT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Button input for changement
     GPIO.setup(BUTTON_REGLAGE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Button input for reglage
     GPIO.setup(BUTTON_ORGANISATION_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Button input for organisation
+    GPIO.setup(VIBRATION_PIN, GPIO.IN)  # Set GPIO16 as vibration input
 
--- vibration.py
+++ vibration.py
@@ -1,20 +1,30 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set up GPIO for vibration sensor
+# Set up GPIO
 GPIO.setmode(GPIO.BCM)  # Use BCM numbering
 GPIO.setup(16, GPIO.IN)  # Set GPIO16 as input
+is_machine_on = False
 
-def check_vibration(detection_values, start_time):
-    vibration_detected = GPIO.input(16)
-    detection_values.append(vibration_detected)
+try:
+    detection_values = []
+    start_time = time.time()
 
-    if time.time() - start_time >= 10:
-        mean_value = sum(detection_values) / len(detection_values)
-        detection_values = []  # Reset the list
-        start_time = time.time()  # Reset the timer
-        is_machine_on = mean_value < 1
-    else:
-        is_machine_on = None  # No update yet within the 10-second window
+    while True:
+        vibration_detected = GPIO.input(16)
+        detection_values.append(vibration_detected)
 
-    return is_machine_on, detection_values, start_time
+        if time.time() - start_time >= 10:
+            mean_value = sum(detection_values) / len(detection_values)
+            print("Mean value detected: {:.2f}".format(mean_value))
+            detection_values = []  # Reset the list
+            start_time = time.time()  # Reset the timer
+            if mean_value ==1 :
+                is_machine_on = False
+            else:
+                is_machine_on = True
+except KeyboardInterrupt:
+    print("Exiting program")
+    
+finally:
+    GPIO.cleanup()  # Clean up GPIO settings
