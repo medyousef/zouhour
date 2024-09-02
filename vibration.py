e349eb815fa44763288414b65c8fbe4752d0eb6b
jojo_golden_wings <jozeftunisino@gmail.com>
2024-09-03 00:18:45 +0200

    pause/panne soustraire

--- vibration.py
+++ vibration.py
@@ -3,13 +3,21 @@ import time
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)  # Use BCM numbering
-GPIO.setup(16, GPIO.IN)  # Set GPIO4 as input
+GPIO.setup(16, GPIO.IN)  # Set GPIO16 as input
 
 try:
+    detection_values = []
+    start_time = time.time()
+
     while True:
         vibration_detected = GPIO.input(16)
-            #print(1)  # Vibration detected
-        print("value detected " + str(vibration_detected))
+        detection_values.append(vibration_detected)
+
+        if time.time() - start_time >= 5:
+            mean_value = sum(detection_values) / len(detection_values)
+            print("Mean value detected: {:.2f}".format(mean_value))
+            detection_values = []  # Reset the list
+            start_time = time.time()  # Reset the timer
 
 except KeyboardInterrupt:
     print("Exiting program")
