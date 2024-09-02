571b443cf9a61061103a684b70673e1dcb473c4e
jojo_golden_wings <jozeftunisino@gmail.com>
2024-09-03 00:07:51 +0200

    is_machine_on

--- vibration.py
+++ vibration.py
@@ -4,7 +4,6 @@ import time
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)  # Use BCM numbering
 GPIO.setup(16, GPIO.IN)  # Set GPIO16 as input
-is_machine_on = False
 
 try:
     detection_values = []
@@ -14,15 +13,12 @@ try:
         vibration_detected = GPIO.input(16)
         detection_values.append(vibration_detected)
 
-        if time.time() - start_time >= 3:
+        if time.time() - start_time >= 2:
             mean_value = sum(detection_values) / len(detection_values)
             print("Mean value detected: {:.2f}".format(mean_value))
             detection_values = []  # Reset the list
             start_time = time.time()  # Reset the timer
-            if mean_value ==1 :
-                is_machine_on = False
-            else:
-                is_machine_on = True
+
 except KeyboardInterrupt:
     print("Exiting program")
     
